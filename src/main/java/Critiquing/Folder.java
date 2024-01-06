695c42a8d91e74c584bfc5be815a30836ff52b3e
Gautam Narayan <gau.narayan@gmail.com>
2024-01-06 15:30:29 -0500

    created classes and started commenting

---
+++ src/main/java/Critiquing/Folder.java
@@ -0,0 +1,9 @@
+package Critiquing;
+
+/**
+ * This class will represent any operations we want to perform involving folders
+ *
+ * We will perform various operations on folders from compressing, compiling, and creating just to name a few.
+ */
+public class Folder {
+}
---
+++ src/main/java/Critiquing/Media.java
@@ -0,0 +1,5 @@
+package Critiquing;
+
+public interface Media {
+
+}
