0deae8c6477fc73d10c06c33c87857a6779c92e9
Gautam Narayan <gau.narayan@gmail.com>
2024-01-10 20:40:34 -0500

    added methods for video function

--- src/main/java/Critiquing/Video.java
+++ src/main/java/Critiquing/Video.java
@@ -2,9 +2,18 @@ package Critiquing;
 
 /**
  * This class will represent any operations we want to perform involving videos
- *
+ * <p>
  * Our main functionality to implement in this class will involve trimming a video into numerous other videos.
  */
-public final class Video {
+public final class Video implements Media<Video> {
 
+    @Override
+    public Video convertTo(String extension) {
+        return null;
+    }
+
+    @Override
+    public void cutAndDownload(String media, String inputPath, String outputPath, int... time) {
+
+    }
 }
