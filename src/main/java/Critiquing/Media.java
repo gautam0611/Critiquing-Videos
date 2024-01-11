61cddd7a2345f43f03efb3c2efa4717ced02e927
Gautam Narayan <gau.narayan@gmail.com>
2024-01-10 20:38:32 -0500

    creating our interfaces

--- src/main/java/Critiquing/Media.java
+++ src/main/java/Critiquing/Media.java
@@ -1,5 +1,29 @@
 package Critiquing;
 
-public interface Media {
+/**
+ * Represents an interface containing methods for any class involving manipulating media.
+ *
+ * @param <T> our Media type which will be either a screenshot or a video.
+ */
+public interface Media<T> {
+
+    /**
+     * A method used to convert a specified type to a specific extension
+     *
+     * @param extension the extension that we want to convert our media type to
+     * @return The specified media type with the specified extension.
+     */
+    public T convertTo(String extension);
+
+    /**
+     * A method used to download videos or screenshots to a specific location
+     *
+     * @param media      will be "FFmepg" if we are performing actions regarding videos
+     * @param inputPath  the input file path that we want to get the file from
+     * @param outputPath the output file path that we want to download to
+     * @param time       a varag that
+     */
+    public void cutAndDownload(String media, String inputPath, String outputPath, int... time);
+
 
 }
--- src/main/java/Critiquing/Screenshot.java
+++ src/main/java/Critiquing/Screenshot.java
@@ -2,9 +2,19 @@ package Critiquing;
 
 /**
  * This class will represent any operations we want to perform involving screenshots
- *
+ * <p>
  * Our main functionalities to implement in this class will involve getting screenshots from a video in either milliseconds
  * or seconds.
  */
-public final class Screenshot {
+public final class Screenshot implements Media<Screenshot> {
+
+    @Override
+    public Screenshot convertTo(String extension) {
+        return null;
+    }
+
+    @Override
+    public void cutAndDownload(String media, String inputPath, String outputPath, int... time) {
+
+    }
 }
