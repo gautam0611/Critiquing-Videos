57879b9d2e6ff274a1f8bdc68e1b65b25f976fed
Gautam Narayan <gau.narayan@gmail.com>
2024-03-17 21:01:23 -0400

    adding tests

--- src/test/java/Critiquing/TimestampTest.java
+++ src/test/java/Critiquing/TimestampTest.java
@@ -2,108 +2,26 @@ package Critiquing;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TimestampTest {
 
     @Test
     void getMinute() {
+        Timestamp t1 = new Timestamp.Builder().setMinute(1).build();
+        assertEquals(t1.getMinute(), 1);
     }
 
     @Test
     void getSeconds() {
+        Timestamp t1 = new Timestamp.Builder().setSeconds(30).build();
+        assertEquals(t1.getSeconds(), 30);
     }
 
     @Test
     void getMilliseconds() {
+        Timestamp t1 = new Timestamp.Builder().setMilliseconds(0.15).build();
+        assertEquals(t1.getMilliseconds(), 15);
     }
 
-//    @Test
-//    void parseTimestampHasIncorrectValues() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-////        Timestamp ts2 = ts1.parseTimestamp("abc");
-//        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
-//            ts1.parseTimestamp("abcd");
-//        }, "Not a valid timestamp");
-//
-//        System.out.println(thrown.getMessage());
-//
-//        assertEquals("Not a valid timestamp", thrown.getMessage());
-//    }
-//
-//    @Test
-//    void parseTimestampSplitStringHasInvalidValues() {
-//
-//    }
-//
-//    @Test
-//    void parseTimestampContainsPeriodButInvalidValues() {
-//
-//    }
-//
-//    @Test
-//    void parseTimestampContainsColonButInvalidValues() {
-//
-//    }
-//
-//    @Test
-//    void parseTimestampContainsColonAndPeriodButInvalidValues() {
-//
-//    }
-//
-//
-//    @Test
-//    void parseTimestampJustSeconds() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("0:20");
-//        assertEquals(0, ts2.getMinute());
-//        assertEquals(20, ts2.getSeconds());
-//    }
-//
-//    @Test
-//    void parseTimestampJustMinute() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("1:00");
-//        assertEquals(1, ts2.getMinute());
-//        assertEquals(0, ts2.getSeconds());
-//    }
-//
-//    @Test
-//    void parseTimestampJustMilliseconds() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("0.20");
-//        assertEquals(0, ts2.getSeconds());
-//        assertEquals(20, ts2.getMilliseconds());
-//    }
-//
-//    @Test
-//    void parseTimestampMinuteAndSeconds() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("1:25");
-//        assertEquals(1, ts2.getMinute());
-//        assertEquals(25, ts2.getSeconds());
-//    }
-//
-//    @Test
-//    void parseTimestampSecondsAndMilliseconds() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("0:25.26");
-//        assertEquals(25, ts2.getSeconds());
-//        assertEquals(26, ts2.getMilliseconds());
-//    }
-//
-//    @Test
-//    void parseTimestampMinuteAndMilliseconds() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp ts2 = ts1.parseTimestamp("1:00.26");
-//        assertEquals(1, ts2.getMinute());
-//        assertEquals(0, ts2.getSeconds());
-//        assertEquals(26, ts2.getMilliseconds());
-//    }
-//
-//    @Test
-//    void calculateDuration() {
-//        Timestamp ts1 = new Timestamp.Builder().build();
-//        Timestamp start = ts1.parseTimestamp("1:26");
-//        Timestamp end = ts1.parseTimestamp("1:30");
-//        assertEquals(4, ts1.calculateDuration(start, end));
-//    }
 }
